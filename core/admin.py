0288ab8f88156f8bfcd97ed033ed5b9cdbb4135f
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2025-07-12 16:37:47 +0530

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile,Item,ItemImage,Swap
-# Register your models here.
-
-admin.site.register(Profile)
-admin.site.register(Item)
-admin.site.register(ItemImage)
-admin.site.register(Swap)
